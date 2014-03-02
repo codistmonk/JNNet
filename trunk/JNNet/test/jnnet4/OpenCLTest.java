5c153de9d7ba13459068062014254f9cd2241bfe
codistmonk <codistmonk@7703c365-f04f-438f-9f75-10fb49d7744b>
2014-03-02 13:01:22 +0000

    [JNNet] Updated OpenCLTest.

--- trunk/JNNet/test/jnnet4/OpenCLTest.java
+++ trunk/JNNet/test/jnnet4/OpenCLTest.java
@@ -4,6 +4,7 @@ import static java.lang.Math.exp;
 import static java.lang.Math.log;
 import static java.lang.Math.min;
 import static java.util.concurrent.Executors.newFixedThreadPool;
+import static jnnet.DLLTools.loadDLL;
 import static jnnet.Neuron.sigmoid;
 import static net.sourceforge.aprog.tools.SystemProperties.getAvailableProcessorCount;
 import static net.sourceforge.aprog.tools.Tools.debugPrint;
@@ -20,13 +21,17 @@ import net.sourceforge.aprog.tools.TicToc;
 
 import org.junit.Test;
 
+import com.amd.aparapi.Kernel;
+import com.amd.aparapi.Range;
+import com.amd.aparapi.Kernel.EXECUTION_MODE;
+
 /**
  * @author codistmonk (creation 2014-03-02)
  */
 public final class OpenCLTest {
 	
 	@Test
-	public final void testSequential() {
+	public final void test1() {
 		final TicToc timer = new TicToc();
 		double sum = 0.0;
 		
@@ -40,7 +45,7 @@ public final class OpenCLTest {
 	}
 	
 	@Test
-	public final void testThreadPool() throws Exception {
+	public final void test2() throws Exception {
 		final int n = getAvailableProcessorCount();
 		final ExecutorService executor = newFixedThreadPool(n);
 		
@@ -54,14 +59,15 @@ public final class OpenCLTest {
 			final double[] sums = new double[n];
 			
 			for (int i = 0; i < N; i += step) {
-				final int taskId = i / step;
 				final int start = i;
-				final int end = min(N, start + step);
 				
 				tasks.add(executor.submit(new Runnable() {
 					
 					@Override
 					public final void run() {
+						final int taskId = start / step;
+						final int end = min(N, start + step);
+						
 						for (int i = start; i < end; ++i) {
 							sums[taskId] += f(i);
 						}
@@ -83,12 +89,68 @@ public final class OpenCLTest {
 		}
 	}
 	
-	public static final double f(final int i) {
-		// TODO find a better slowing trick...
-		return exp(log(exp(log(exp(log(sigmoid(WEIGHTS[i] * SOURCES[i])))))));
+	@Test
+	public final void test3() throws Exception {
+		loadDLL("aparapi");
+		
+		final TicToc timer = new TicToc();
+		
+		debugPrint(new Date(timer.tic()));
+		
+		final int step = N / 2000;
+		final int n = (N + step - 1) / step;
+		final double[] sums = new double[n];
+		
+		final Kernel kernel = new Kernel() {
+			
+			private final double[] weights = WEIGHTS;
+			
+			private final double[] sources = SOURCES;
+			
+			@Override
+			public final void run() {
+				final int globalId = this.getGlobalId();
+				final int start = globalId * step;
+				final int end = min(N, start + step);
+				
+				for (int i = start; i < end; ++i) {
+					sums[globalId] += f(this.weights, this.sources, i);
+				}
+			}
+			
+		};
+		
+		try {
+			final Range range = Range.create(n);
+			
+			debugPrint(range.getGlobalSize(0), range.getLocalSize(0),
+					range.getWorkGroupSize(), range.getNumGroups(0));
+			
+			kernel.setExecutionMode(EXECUTION_MODE.GPU);
+			kernel.execute(range);
+			
+			double sum = 0.0;
+			
+			for (final double value : sums) {
+				sum += value;
+			}
+			
+			debugPrint(getThisMethodName(), "sum", sum, "time:", timer.toc());
+		} finally {
+			kernel.dispose();
+		}
 	}
 	
-	public static final int N = 100000000;
+	public static final double f(final int i) {
+		return f(WEIGHTS, SOURCES, i);
+	}
+	
+	public static final double f(final double[] weights, final double[] sources, final int i) {
+		// TODO find a better slowing trick...
+		return exp(log(exp(log(exp(log(sigmoid(weights[i] * sources[i])))))));
+	}
+	
+	public static final int N = 10000000;
 	
 	public static final double[] WEIGHTS = new double[N];
 	
