e63f11c2b7d93e86a668e5a3d57587eaa946b9c8
codistmonk <codistmonk@7703c365-f04f-438f-9f75-10fb49d7744b>
2014-03-10 13:26:13 +0000

    [JNNet][jnnet4] Updated BinaryClassifierTest.

--- JNNet/test/jnnet4/BinaryClassifierTest.java
+++ JNNet/test/jnnet4/BinaryClassifierTest.java
@@ -7,6 +7,7 @@ import static jnnet4.VectorStatistics.add;
 import static jnnet4.VectorStatistics.dot;
 import static jnnet4.VectorStatistics.scaled;
 import static jnnet4.VectorStatistics.subtract;
+import static net.sourceforge.aprog.tools.Factory.DefaultFactory.HASH_SET_FACTORY;
 import static net.sourceforge.aprog.tools.Tools.debugPrint;
 import static net.sourceforge.aprog.tools.Tools.instances;
 import static org.junit.Assert.*;
@@ -14,8 +15,11 @@ import static org.junit.Assert.*;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.BitSet;
+import java.util.Collection;
 import java.util.List;
 
+import jnnet.DoubleList;
 import net.sourceforge.aprog.tools.Factory;
 import net.sourceforge.aprog.tools.Factory.DefaultFactory;
 
@@ -28,7 +32,16 @@ public final class BinaryClassifierTest {
 	
 	@Test
 	public final void test() {
-		fail("Not yet implemented");
+		final Dataset trainingData = new Dataset("jnnet/2spirals.txt");
+		final BinaryClassifier classifier = new BinaryClassifier(trainingData);
+		
+		debugPrint(classifier.getClusters().size());
+		
+		final SimpleConfusionMatrix confusionMatrix = classifier.evaluate(trainingData);
+		
+		debugPrint(confusionMatrix);
+		
+		assertEquals(0, confusionMatrix.getTotalErrorCount());
 	}
 	
 }
@@ -37,6 +50,99 @@ public final class BinaryClassifierTest {
  * @author codistmonk (creation 2014-03-10)
  */
 final class BinaryClassifier implements Serializable {
+	
+	private final int step;
+	
+	private final double[] hyperplanes;
+	
+	private final Collection<BitSet> clusters;
+	
+	private final boolean invertOutput;
+	
+	public BinaryClassifier(final Dataset dataset) {
+		final DoubleList hyperplanes = new DoubleList();
+		
+		generateHyperplanes(dataset, new HyperplaneHandler() {
+			
+			@Override
+			public final boolean hyperplane(final double bias, final double[] weights) {
+				hyperplanes.add(bias);
+				hyperplanes.addAll(weights);
+				
+				return true;
+			}
+			
+		});
+		
+		final int step = dataset.getStep();
+		final double[] data = dataset.getData();
+		final int dataLength = data.length;
+		final Collection<BitSet>[] codes = instances(2, HASH_SET_FACTORY);
+		this.step = step;
+		this.hyperplanes = hyperplanes.toArray();
+		
+		for (int i = 0; i < dataLength; i += step) {
+			final double[] item = copyOfRange(data, i, i + step - 1);
+			final int label = (int) data[i + step - 1];
+			final BitSet code = this.encode(item);
+			
+			codes[label].add(code);
+		}
+		
+		// TODO prune hyperplane
+		
+		this.invertOutput = codes[0].size() < codes[1].size();
+		this.clusters = this.invertOutput ? codes[0] : codes[1];
+	}
+	
+	public final BitSet encode(final double[] item) {
+		final int weightCount = this.hyperplanes.length;
+		final int hyperplaneCount = weightCount / this.step;
+		final BitSet code = new BitSet(hyperplaneCount);
+		
+		for (int j = 0, bit = 0; j < weightCount; j += this.step, ++bit) {
+			final double d = this.hyperplanes[j] + dot(item, copyOfRange(this.hyperplanes, j + 1, j + this.step));
+			
+			code.set(bit, 0.0 <= d);
+		}
+		
+		return code;
+	}
+	
+	public final Collection<BitSet> getClusters() {
+		return this.clusters;
+	}
+	
+	public final boolean accept(final double... item) {
+		return this.getClusters().contains(this.encode(item)) ^ this.invertOutput;
+	}
+	
+	public final SimpleConfusionMatrix evaluate(final Dataset trainingData) {
+		final SimpleConfusionMatrix result = new SimpleConfusionMatrix();
+		final int step = trainingData.getStep();
+		final double[] data = trainingData.getData();
+		
+		for (int i = 0; i < data.length; i += step) {
+			final double expected = data[i + step - 1];
+			final double actual = this.accept(copyOfRange(data, i, i + step - 1)) ? 1.0 : 0.0;
+			
+			if (expected != actual) {
+				if (expected == 1.0) {
+					result.getFalseNegativeCount().incrementAndGet();
+				} else {
+					result.getFalsePositiveCount().incrementAndGet();
+				}
+			} else {
+				if (expected == 1.0) {
+					result.getTruePositiveCount().incrementAndGet();
+				} else {
+					result.getTrueNegativeCount().incrementAndGet();
+				}
+			}
+		}
+		
+		return result;
+	}
 	
 	/**
 	 * {@value}.
