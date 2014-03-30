65f96f265bc93e249b8b58b35ba067930d9ea250
codistmonk <codistmonk@7703c365-f04f-438f-9f75-10fb49d7744b>
2014-03-30 12:49:40 +0000

    [JNNet][jnnet4] Updated LinearConstraintSolverTest.

--- trunk/JNNet/test/jnnet4/LinearConstraintSystemTest.java
+++ trunk/JNNet/test/jnnet4/LinearConstraintSystemTest.java
@@ -11,10 +11,10 @@ import static org.junit.Assert.*;
 
 import java.io.Serializable;
 import java.util.Arrays;
-import java.util.Random;
 
 import jnnet.DoubleList;
 import jnnet.IntList;
+
 import net.sourceforge.aprog.tools.TicToc;
 import net.sourceforge.aprog.tools.Tools;
 
@@ -248,12 +248,12 @@ public final class LinearConstraintSystemTest {
 			final double[] data = this.getData().toArray();
 			final int order = this.getOrder();
 			final double[] result = copyOf(data, order);
+			final TicToc timer = new TicToc();
+			int remainingIterations = 10;
 			
 			if (algo == 0) {
 				return this.solve();
 			} else if (algo == 1) {
-				final TicToc timer = new TicToc();
-				int remainingIterations = 1000000;
 				boolean done;
 				
 				timer.tic();
@@ -289,93 +289,117 @@ public final class LinearConstraintSystemTest {
 				debugPrint(remainingIterations);
 				
 				return unscale(result);
-			}
-			
-			for (int i = order; i < data.length; i += order) {
+			} else if (algo == 2) {
+				for (int i = order; i < data.length; i += order) {
 //				debugPrint(Arrays.toString(result));
-				double alpha = 0.0;
-				final double denominatorI = dot(result, 0, data, i, order);
-				
-				if (0.0 < denominatorI) {
-					alpha = -dot(data, i, data, i, order) / denominatorI;
-				}
-				
-//				debugPrint(i / order, alpha, dot(data, i, data, i, order), denominatorI);
-				
-				for (int j = 0; j < i; j += order) {
-					double denominatorJ = dot(result, 0, data, j, order);
+					double alpha = 0.0;
+					final double denominatorI = dot(result, 0, data, i, order);
 					
-					if (denominatorJ < 0.0) {
-//						debugPrint(i / order, j / order, denominatorJ);
-						denominatorJ = 0.0;
+					if (0.0 < denominatorI) {
+						alpha = -dot(data, i, data, i, order) / denominatorI;
 					}
 					
-					if (0.0 != denominatorJ) {
-						final double ratio = -dot(data, i, data, j, order) / denominatorJ;
+//				debugPrint(i / order, alpha, dot(data, i, data, i, order), denominatorI);
+					
+					for (int j = 0; j < i; j += order) {
+						double denominatorJ = dot(result, 0, data, j, order);
 						
-						if (alpha < ratio) {
-							alpha = ratio;
+						if (denominatorJ < 0.0) {
+//						debugPrint(i / order, j / order, denominatorJ);
+							denominatorJ = 0.0;
+						}
+						
+						if (0.0 != denominatorJ) {
+							final double ratio = -dot(data, i, data, j, order) / denominatorJ;
 							
+							if (alpha < ratio) {
+								alpha = ratio;
+								
 //							debugPrint(i / order, j / order, alpha, dot(data, i, data, j, order), denominatorJ);
+							}
+						}
+					}
+					
+					if (0.0 <= denominatorI) {
+						alpha = alpha + 0.5;
+					} else {
+						double maxAlpha = -dot(data, i, data, i, order) / denominatorI;
+						
+						if (maxAlpha <= alpha) {
+							debugPrint(i / order, alpha, maxAlpha);
+//						throw new IllegalStateException();
+						}
+						
+						alpha = (alpha + maxAlpha) / 2.0;
+					}
+					
+					for (int j = 0; j < order; ++j) {
+						result[j] = alpha * result[j] + data[i + j];
+					}
+					
+//				debugPrint(alpha);
+					
+					for (int j = 0; j < i; j += order) {
+						if (dot(result, 0, data, j, order) < 0.0) {
+//						debugPrint(j / order, dot(result, 0, data, j, order), denominatorI, alpha);
+							
+//						throw new IllegalStateException();
 						}
 					}
 				}
 				
-				if (0.0 <= denominatorI) {
-					alpha = alpha + 0.5;
-				} else {
-					double maxAlpha = -dot(data, i, data, i, order) / denominatorI;
-					
-					if (maxAlpha <= alpha) {
-						debugPrint(i / order, alpha, maxAlpha);
-//						throw new IllegalStateException();
-					}
-					
-					alpha = (alpha + maxAlpha) / 2.0;
-				}
-				
-				for (int j = 0; j < order; ++j) {
-					result[j] = alpha * result[j] + data[i + j];
-				}
-				
-//				debugPrint(alpha);
-				
-				for (int j = 0; j < i; j += order) {
-					if (dot(result, 0, data, j, order) < 0.0) {
-//						debugPrint(j / order, dot(result, 0, data, j, order), denominatorI, alpha);
-						
-//						throw new IllegalStateException();
-					}
-				}
-			}
-			
 //			debugPrint(Arrays.toString(result));
-			
-			return result;
-		}
-		
-		public static final double[] unscale(final double[] v) {
-			final double scale = v[0];
-			
-			if (scale != 0.0) {
-				final int n = v.length;
 				
-				for (int i = 0; i < n; ++i) {
-					v[i] /= scale;
+				return result;
+			} else if (algo == 3) {
+				timer.tic();
+				
+				final int extendedOrder = order + 1;
+				final int extraDimension = order;
+				final double[] extendedData = new double[data.length / order * extendedOrder];
+				
+				for (int i = 0, j = 0; i < data.length; i += order, j += extendedOrder) {
+					System.arraycopy(data, i, extendedData, j, order);
+					extendedData[j + extraDimension] = -100.0;
 				}
+				
+				final double[] extendedPoint = new double[extendedOrder];
+				
+				extendedPoint[0] = 1.0;
+				
+				int unsatisfiedConstraintOffset = lockSatisfiedConstraints(extendedData, extendedPoint);
+				
+				while (0 <= unsatisfiedConstraintOffset && 0 <= --remainingIterations) {
+					debugPrint(Arrays.toString(extendedPoint));
+					if (5000L <= timer.toc()) {
+						debugPrint("remainingIterations:", remainingIterations, "extendedPoint[extraDimension]:", extendedPoint[extraDimension]);
+						timer.tic();
+					}
+					
+					final int lowestConstraintOffset = solveUsingLastDimension(extendedData, extendedPoint);
+					
+					checkSolution(extendedData, extendedPoint);
+					
+					moveAlongConstraintToLastDimension0(extendedData, lowestConstraintOffset, extendedPoint);
+					
+					unscale(extendedPoint);
+					
+					if (!softCheckSolution(extendedData, extendedPoint)) {
+						debugPrint();
+						break;
+					}
+					
+					unsatisfiedConstraintOffset = lockSatisfiedConstraints(extendedData, extendedPoint);
+				}
+				
+//				debugPrint(Arrays.toString(extendedPoint));
+				
+				System.arraycopy(unscale(extendedPoint), 0, result, 0, order);
+				
+				return result;
 			}
 			
-			return v;
-		}
-		
-		public static final double dot(final double[] data1, final int offset1, final double[] data2, final int offset2, final int n) {
-			double result = 0.0;
-			
-			for (int i = 0; i < n; ++i) {
-				result += data1[offset1 + i] * data2[offset2 + i];
-			}
-			
-			return result;
+			throw new IllegalStateException();
 		}
 		
 		public final double[] solve() {
@@ -439,7 +463,7 @@ public final class LinearConstraintSystemTest {
 			final int order = this.getOrder();
 			final int extendedOrder = extendedPoint.length;
 			
-			checkSolution(extendedData, extendedOrder, extendedPoint);
+			checkSolution(extendedData, extendedPoint);
 			
 			final IntList limitIds = new IntList();
 			final double[] extendedDirection = new double[extendedOrder];
@@ -485,14 +509,14 @@ public final class LinearConstraintSystemTest {
 					}
 				}
 				
-				checkSolution(extendedData, extendedOrder, extendedPoint);
+				checkSolution(extendedData, extendedPoint);
 				
 				if (!Double.isInfinite(smallestDisplacement) && EPSILON < smallestDisplacement) {
 					for (int i = 1; i < extendedOrder; ++i) {
 						extendedPoint[i] += smallestDisplacement * extendedDirection[i];
 					}
 					
-					checkSolution(extendedData, extendedOrder, extendedPoint);
+					checkSolution(extendedData, extendedPoint);
 					
 					return true;
 				}
@@ -513,15 +537,138 @@ public final class LinearConstraintSystemTest {
 		 */
 		public static final double EPSILON = 1E-7;
 		
-		public static final void checkSolution(final double[] data, final int order, final double[] point) {
+		public static final int solveUsingLastDimension(final double[] data, final double[] point) {
+			int result = -1;
+			final int n = data.length;
+			final int order = point.length;
+			final int last = order - 1;
+			
+			for (int i = 0; i < n; i += order) {
+				final double value = dot(data, i, point, 0, order);
+				
+				if (value < 0.0) {
+					// (point + k * (0 .. 0, 1)) . h = 0
+					// <- value + k * h[last] = 0
+					// <- k = -value / h[last]
+					point[last] -= value / data[i + last];
+					
+					debugPrint(Arrays.toString(point), dot(data, i, point, 0, order));
+					
+					result = i;
+				}
+			}
+			
+			debugPrint("lowest:", result / order);
+			
+			return result;
+		}
+		
+		public static final void moveAlongConstraintToLastDimension0(final double[] data, final int constraintOffset, final double[] point) {
+			final int order = point.length;
+			final int last = order - 1;
+			final double[] objective = new double[order];
+			
+			objective[last] = 1.0;
+			
+			// direction . h = 0
+			// (objective + k1 * h) . h = 0
+			// h[last] + k1 * h . h = 0
+			// k1 = -h[last] / h . h
+			final double k1 = -data[constraintOffset + last] / dot(data, constraintOffset, data, constraintOffset, order);
+			
+			// (point')[last] = 0
+			// (point + k2 * direction)[last] = 0
+			// point[last] + k2 * direction[last] = 0
+			// k2 = -point[last] / direction[last]
+			// k2 = -point[last] / (objective + k1 * h)[last]
+			// k2 = -point[last] / (1 + k1 * h[last])
+			final double k2 = -point[last] / (1.0 + k1 * data[constraintOffset + last]);
+			
+//			debugPrint(Arrays.toString(unscale(point)));
+//			debugPrint(constraintOffset / order);
+			
+			for (int i = 0; i < order; ++i) {
+				point[i] += k2 * (objective[i] + k1 * data[constraintOffset + i]);
+			}
+			
+//			debugPrint(Arrays.toString(unscale(point)));
+			debugPrint(point[last], dot(data, constraintOffset, point, 0, order));
+			debugPrint("satisfied:", constraintOffset / order);
+		}
+		
+		public static final double[] unscale(final double[] v) {
+			final double scale = v[0];
+			
+			if (scale != 0.0) {
+				final int n = v.length;
+				
+				for (int i = 0; i < n; ++i) {
+					v[i] /= scale;
+				}
+			}
+			
+			return v;
+		}
+		
+		public static final double dot(final double[] data1, final int offset1, final double[] data2, final int offset2, final int n) {
+			double result = 0.0;
+			
+			for (int i = 0; i < n; ++i) {
+				result += data1[offset1 + i] * data2[offset2 + i];
+			}
+			
+			return result;
+		}
+		
+		public static final int lockSatisfiedConstraints(final double[] data, final double[] point) {
+			int result = -1;
+			final int order = point.length;
+			final int lastDimension = order - 1;
+			
+			for (int i = 0; i < data.length; i += order) {
+				final double value = evaluate(data, order, i / order, point);
+				
+				if (-EPSILON <= value) {
+					debugPrint("locked:", i / order);
+					data[lastDimension] = 0.0;
+				} else if (result < 0) {
+					debugPrint("unsatisfied:", i / order, result);
+					result = i;
+				}
+			}
+			
+			return result;
+		}
+		
+		public static final void checkSolution(final double[] data, final double[] point) {
+			final int order = point.length;
+			
 			for (int i = 0; i < data.length; i += order) {
 				final double value = evaluate(data, order, i / order, point);
 				
 				if (value + EPSILON < 0.0) {
 					debugPrint(i, i / order, value);
+					
 					throw new IllegalStateException();
 				}
 			}
+		}
+		
+		public static final boolean softCheckSolution(final double[] data, final double[] point) {
+			final int order = point.length;
+			final int last = order - 1;
+			
+			for (int i = 0; i < data.length; i += order) {
+				if (data[i + last] == 0.0) {
+					final double value = evaluate(data, order, i / order, point);
+					
+					if (value + EPSILON < 0.0) {
+						return false;
+					}
+				}
+			}
+			
+			return true;
 		}
 		
 		public static final double vectorNorm(final double[] data, final int offset, final int order) {
