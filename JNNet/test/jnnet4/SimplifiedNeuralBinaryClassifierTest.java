f9f92ba7e603dde4f2260e500cc90fe99a802d4e
codistmonk <codistmonk@7703c365-f04f-438f-9f75-10fb49d7744b>
2014-04-07 08:12:26 +0000

    [JNNet][jnnet4] Refactoring.

--- JNNet/test/jnnet4/SimplifiedNeuralBinaryClassifierTest.java
+++ JNNet/test/jnnet4/SimplifiedNeuralBinaryClassifierTest.java
@@ -1,7 +1,5 @@
 package jnnet4;
 
-import static java.lang.Double.parseDouble;
-import static java.lang.Math.ceil;
 import static java.lang.Math.max;
 import static java.lang.Math.min;
 import static java.lang.Math.sqrt;
@@ -20,7 +18,6 @@ import static jnnet4.VectorStatistics.scaled;
 import static jnnet4.VectorStatistics.subtract;
 import static net.sourceforge.aprog.tools.Factory.DefaultFactory.HASH_MAP_FACTORY;
 import static net.sourceforge.aprog.tools.Factory.DefaultFactory.HASH_SET_FACTORY;
-import static net.sourceforge.aprog.tools.Tools.DEBUG_STACK_OFFSET;
 import static net.sourceforge.aprog.tools.Tools.array;
 import static net.sourceforge.aprog.tools.Tools.debug;
 import static net.sourceforge.aprog.tools.Tools.debugPrint;
@@ -35,9 +32,6 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintStream;
 import java.io.Serializable;
 import java.lang.reflect.Array;
 import java.lang.reflect.Method;
@@ -47,11 +41,9 @@ import java.util.BitSet;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -61,6 +53,7 @@ import jnnet.DoubleList;
 import jnnet4.BinaryClassifier.EvaluationMonitor;
 import jnnet4.LinearConstraintSystemTest.LinearConstraintSystem;
 import jnnet4.LinearConstraintSystemTest.OjAlgoLinearConstraintSystem;
+
 import net.sourceforge.aprog.swing.SwingTools;
 import net.sourceforge.aprog.tools.Factory;
 import net.sourceforge.aprog.tools.IllegalInstantiationException;
@@ -75,7 +68,7 @@ import org.junit.Test;
  */
 public final class SimplifiedNeuralBinaryClassifierTest {
 	
-//	@Test
+	@Test
 	public final void test1() {
 		final boolean showClassifier = true;
 		final boolean previewTrainingData = false;
@@ -271,326 +264,6 @@ public final class SimplifiedNeuralBinaryClassifierTest {
 //		assertEquals(0, confusionMatrix.getTotalErrorCount());
 	}
 	
-	@Test
-	public final void test3() throws Exception {
-		if (false) {
-			debugPrint(ImageIO.read(new File("F:/icpr2014_mitos_atypia/A18/frames/x40/A18_00Cc.png")));
-			
-			if (true) return;
-		}
-		
-		final String root = "F:/icpr2014_mitos_atypia/";
-		final PrintStream out = new PrintStream(new File(root, "A.data"));
-		
-		try {
-			for (final String imageSetId : array("A03", "A04", "A05", "A07", "A10", "A11", "A12", "A14", "A15", "A17", "A18")) {
-				final String imageSetRoot = root + imageSetId;
-				final File frames40 = new File(imageSetRoot + "/frames/x40/");
-				final File mitosis = new File(imageSetRoot + "/mitosis/");
-				final Map<String, VirtualImage40> images = new HashMap<String, VirtualImage40>();
-				
-				try {
-					for (final String file : frames40.list()) {
-						final String tileId = file.replaceAll("\\..+$", "");
-						final String imageBasePath = new File(frames40, tileId.substring(0, tileId.length() - 2)).toString();
-						
-						debugPrint(tileId, imageBasePath);
-						
-						final VirtualImage40 image = getOrCreate(images, imageBasePath,
-								new DefaultFactory<VirtualImage40>(VirtualImage40.class, imageBasePath));
-						
-						debugPrint(image.getWidth(), image.getHeight());
-						
-						convert(tileId, image, new File(mitosis, tileId + "_mitosis.csv"), out);
-						convert(tileId, image, new File(mitosis, tileId + "_not_mitosis.csv"), out);
-					}
-				} finally {
-					ImageIO.write(mitosisMosaicBuilder[0].generateMosaic(), "png", new File(mitosis, "not_mitosis_mosaic.png"));
-					ImageIO.write(mitosisMosaicBuilder[1].generateMosaic(), "png", new File(mitosis, "mitosis_mosaic.png"));
-				}
-			}
-		} finally {
-			out.close();
-		}
-	}
-	
-	private static final MosaicBuilder[] mitosisMosaicBuilder = instances(2, DefaultFactory.forClass(MosaicBuilder.class));
-	
-	public static final void convert(final String tileId, final VirtualImage40 image, final File csv, final PrintStream out) throws FileNotFoundException {
-		final Scanner scanner = new Scanner(csv);
-		final int windowHalfSize = 32;
-		
-		try {
-			while (scanner.hasNext()) {
-				final String[] line = scanner.nextLine().split(",");
-				final int x0 = (int) parseDouble(line[0]);
-				final int y0 = (int) parseDouble(line[1]);
-				final double score = parseDouble(line[2]);
-				final int label = score < 0.5 ? 0 : 1;
-				final BufferedImage element = new BufferedImage(2 * windowHalfSize, 2 * windowHalfSize, BufferedImage.TYPE_3BYTE_BGR);
-				
-				for (int y = y0 - windowHalfSize; y < y0 + windowHalfSize; ++y) {
-					for (int x = x0 - windowHalfSize; x < x0 + windowHalfSize; ++x) {
-						final int rgb = image.getRGB(tileId, x, y);
-						
-						element.setRGB(x - (x0 - windowHalfSize), y - (y0 - windowHalfSize), rgb);
-					}
-				}
-				
-				mitosisMosaicBuilder[label].getImages().add(element);
-				
-				// 0°
-				{
-					for (int y = 0; y < 2 * windowHalfSize; ++y) {
-						for (int x = 0; x < 2 * windowHalfSize; ++x) {
-							final Color color = new Color(element.getRGB(x, y));
-							
-							out.print(color.getRed() + " " + color.getGreen() + " " + color.getBlue() + " ");
-						}
-					}
-					
-					out.println(label);
-				}
-				
-				// 90°
-				{
-					for (int x = 0; x < 2 * windowHalfSize; ++x) {
-						for (int y = 2 * windowHalfSize - 1; 0 <= y; --y) {
-							final Color color = new Color(element.getRGB(x, y));
-							
-							out.print(color.getRed() + " " + color.getGreen() + " " + color.getBlue() + " ");
-						}
-					}
-					
-					out.println(label);
-				}
-				
-				// 180°
-				{
-					for (int y = 2 * windowHalfSize - 1; 0 <= y; --y) {
-						for (int x = 2 * windowHalfSize - 1; 0 <= x; --x) {
-							final Color color = new Color(element.getRGB(x, y));
-							
-							out.print(color.getRed() + " " + color.getGreen() + " " + color.getBlue() + " ");
-						}
-					}
-					
-					out.println(label);
-				}
-				
-				// 270°
-				{
-					for (int x = 2 * windowHalfSize - 1; 0 <= x; --x) {
-						for (int y = 0; y < 2 * windowHalfSize; ++y) {
-							final Color color = new Color(element.getRGB(x, y));
-							
-							out.print(color.getRed() + " " + color.getGreen() + " " + color.getBlue() + " ");
-						}
-					}
-					
-					out.println(label);
-				}
-			}
-		} finally {
-			scanner.close();
-		}
-	}
-	
-	/**
-	 * @author codistmonk (creation 2014-03-30)
-	 */
-	public static final class VirtualImage40 implements Serializable {
-		
-		private final String basePath;
-		
-		private final BufferedImage[][] tiles;
-		
-		private final int width;
-		
-		private final int height;
-		
-		public VirtualImage40(final String basePath) {
-			this.basePath = basePath;
-			this.tiles = new BufferedImage[4][4];
-			
-			for (int quad0 = 0; quad0 <= 3; ++quad0) {
-				for (int quad1 = 0; quad1 <= 3; ++quad1) {
-					this.tiles[quad0][quad1] = readTile(quad0, quad1);
-				}
-			}
-			
-			this.width = this.tiles[0][0].getWidth() + this.tiles[0][1].getWidth() + this.tiles[1][0].getWidth() + this.tiles[1][1].getWidth();
-			this.height = this.tiles[0][0].getHeight() + this.tiles[0][2].getHeight() + this.tiles[2][0].getHeight() + this.tiles[2][2].getHeight();
-		}
-		
-		public final int getWidth() {
-			return this.width;
-		}
-		
-		public final int getHeight() {
-			return this.height;
-		}
-		
-		public final int getRGB(final String tileId, final int x, final int y) {
-			int quad0 = tileId.charAt(tileId.length() - 2) - 'A';
-			int quad1 = tileId.charAt(tileId.length() - 1) - 'a';
-			
-			BufferedImage tile = this.tiles[quad0][quad1];
-			int w = tile.getWidth();
-			int h = tile.getHeight();
-			
-			try {
-				if (x < 0) {
-					if (quad1 == 1 || quad1 == 3) {
-						--quad1;
-					} else {
-						++quad1;
-						
-						if (quad0 == 1 || quad0 == 3) {
-							--quad0;
-						} else {
-							throw new IllegalArgumentException();
-						}
-					}
-				} else if (w <= x) {
-					if (quad1 == 0 || quad1 == 2) {
-						++quad1;
-					} else {
-						--quad1;
-						
-						if (quad0 == 0 || quad0 == 2) {
-							++quad0;
-						} else {
-							throw new IllegalArgumentException();
-						}
-					}
-				}
-				
-				if (y < 0) {
-					if (quad1 == 2 || quad1 == 3) {
-						--quad1;
-					} else {
-						++quad1;
-						
-						if (quad0 == 2 || quad0 == 3) {
-							--quad0;
-						} else {
-							throw new IllegalArgumentException();
-						}
-					}
-				} else if (h <= y) {
-					if (quad1 == 0 || quad1 == 1) {
-						++quad1;
-					} else {
-						--quad1;
-						
-						if (quad0 == 0 || quad0 == 1) {
-							++quad0;
-						} else {
-							throw new IllegalArgumentException();
-						}
-					}
-				}
-			} catch (final IllegalArgumentException exception) {
-				ignore(exception);
-				
-				System.err.println(debug(DEBUG_STACK_OFFSET, tileId, x, y));
-				
-				return 0;
-			}
-			
-			tile = this.tiles[quad0][quad1];
-			w = tile.getWidth();
-			h = tile.getHeight();
-			
-			return tile.getRGB((w + x) % w, (h + y) % h);
-		}
-		
-		private final BufferedImage readTile(final int quad0, final int quad1) {
-			final File file = new File(this.basePath + (char) ('A' + quad0) + "" + (char) ('a' + quad1) + ".png");
-			
-			try {
-				return ImageIO.read(file);
-			} catch (final IOException exception) {
-				System.err.println(debug(DEBUG_STACK_OFFSET, "Error reading file", file));
-				throw unchecked(exception);
-			}
-		}
-		
-		/**
-		 * {@value}.
-		 */
-		private static final long serialVersionUID = 5357895005657732226L;
-		
-	}
-	
-	/**
-	 * @author codistmonk (creation 2014-03-28)
-	 */
-	public static final class MosaicBuilder implements Serializable {
-		
-		private final List<BufferedImage> images;
-		
-		private final int preferredRowCount;
-		
-		public MosaicBuilder() {
-			this(0);
-		}
-		
-		public MosaicBuilder(final int preferredRowCount) {
-			this.images = new ArrayList<BufferedImage>();
-			this.preferredRowCount = preferredRowCount;
-		}
-		
-		public final List<BufferedImage> getImages() {
-			return this.images;
-		}
-		
-		public final int getPreferredRowCount() {
-			return this.preferredRowCount;
-		}
-		
-		public final BufferedImage generateMosaic() {
-			if (this.getImages().isEmpty()) {
-				return new BufferedImage(1, 1, BufferedImage.TYPE_3BYTE_BGR);
-			}
-			final int rowCount = 0 < this.getPreferredRowCount() ? this.getPreferredRowCount() :
-				(int) ceil(sqrt(this.getImages().size()));
-			final int columnCount = (int) ceil((double) this.getImages().size() / rowCount);
-			
-			debugPrint(this.getImages().size(), rowCount, columnCount);
-			
-			final int imageWidth = this.getImages().get(0).getWidth();
-			final int imageHeight = this.getImages().get(0).getHeight();
-			final int mosaicWidth = columnCount * imageWidth;
-			final int mosaicHeight = columnCount * imageHeight;
-			final BufferedImage result = new BufferedImage(mosaicWidth, mosaicHeight, BufferedImage.TYPE_3BYTE_BGR);
-			
-			{
-				final Graphics2D g = result.createGraphics();
-				int i = 0;
-				
-				for (final BufferedImage image : this.getImages()) {
-					final int tileX = (i % columnCount) * imageWidth;
-					final int tileY = (i / columnCount) * imageHeight;
-					
-					g.drawImage(image, tileX, tileY, null);
-					++i;
-				}
-				
-				g.dispose();
-			}
-			
-			return result;
-		}
-		
-		/**
-		 * {@value}.
-		 */
-		private static final long serialVersionUID = -3610143890164242310L;
-		
-	}
-	
 	public static final BufferedImage newImage(final double[] example, final int offset, final int w, final int h) {
 		final BufferedImage image = new BufferedImage(w, h, BufferedImage.TYPE_3BYTE_BGR);
 		
@@ -599,6 +272,7 @@ public final class SimplifiedNeuralBinaryClassifierTest {
 				image.setRGB(x, y, rgb(max(0.0, min(example[p] / example[0] / 255.0, 1.0))));
 			}
 		}
+		
 		return image;
 	}
 	
