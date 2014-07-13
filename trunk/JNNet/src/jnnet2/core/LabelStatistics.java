53f4f498ab1005f1c2f04092d3bef8a64b35b8e9
codistmonk <codistmonk@7703c365-f04f-438f-9f75-10fb49d7744b>
2014-07-13 06:32:42 +0000

    [JNNet][jnnet2] Updated PartitioningClassifier.

--- trunk/JNNet/src/jnnet2/core/LabelStatistics.java
+++ trunk/JNNet/src/jnnet2/core/LabelStatistics.java
@@ -3,6 +3,7 @@ package jnnet2.core;
 import static net.sourceforge.aprog.tools.Tools.getOrCreate;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -16,15 +17,32 @@ public final class LabelStatistics implements Serializable {
 	
 	private final Map<Double, VectorStatistics> statistics = new TreeMap<>();
 	
+	private final Map<Double, Integer> labelIds = new HashMap<>();
+	
 	public final LabelStatistics addItem(final double... item) {
 		final int n = item.length;
 		final int labelIndex = n - 1;
 		final int inputSize = labelIndex;
+		final double label = item[labelIndex];
 		
-		getOrCreate(this.getStatistics(), item[labelIndex]
+		getOrCreate(this.getStatistics(), label
 				, DefaultFactory.forClass(VectorStatistics.class, inputSize)).addValues(item);
 		
+		final Integer labelId = this.labelIds.get(label);
+		
+		if (labelId == null) {
+			this.labelIds.put(label, this.labelIds.size());
+		}
+		
 		return this;
+	}
+	
+	public final int getLabelId(final double label) {
+		return this.labelIds.get(label);
+	}
+	
+	public final int getLabelCount() {
+		return this.getStatistics().size();
 	}
 	
 	public final Map<Double, VectorStatistics> getStatistics() {
