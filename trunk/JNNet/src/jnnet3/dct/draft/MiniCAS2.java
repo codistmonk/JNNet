b1d05b5295723d862e7dc13768f8872627e946dd
codistmonk <codistmonk@7703c365-f04f-438f-9f75-10fb49d7744b>
2015-04-06 00:14:37 +0000

    [JNNet][jnnet3][dct][draft] Updated MiniCAS2.

--- trunk/JNNet/src/jnnet3/dct/draft/MiniCAS2.java
+++ trunk/JNNet/src/jnnet3/dct/draft/MiniCAS2.java
@@ -1,11 +1,13 @@
 package jnnet3.dct.draft;
 
-import static java.util.Arrays.stream;
+import static java.util.Arrays.*;
+import static java.util.stream.Collectors.toList;
 import static net.sourceforge.aprog.tools.Tools.*;
 
 import java.io.Serializable;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.function.DoubleSupplier;
 
 import net.sourceforge.aprog.tools.IllegalInstantiationException;
@@ -21,15 +23,93 @@ public final class MiniCAS2 {
 	
 	public static final Value ZERO = new Value(0.0);
 	
+	public static final Value ONE = new Value(1.0);
+	
+	public static final Value MINUS_ONE = new Value(-1.0);
+	
+	public static final Value PI = new Value(Math.PI);
+	
 	/**
 	 * @param commandLineArguments
 	 * <br>Unused
 	 */
 	public static final void main(final String[] commandLineArguments) {
-		final Variable i = new Variable("i");
-		final Sum sum = new Sum(i, i, new Value(10.0));
+		final Variable i = variable("i");
+		final Sum sum = sum(i, i, 10);
+		final Product product = product(add(i, 1), i, 10);
 		
 		debugPrint(sum, "=", sum.getAsDouble());
+		debugPrint(product, "=", product.getAsDouble());
+		debugPrint(add(2, 2), "=", add(2, 2).getAsDouble());
+		debugPrint(sum(multiply("a", cos("x")), i, "n"));
+	}
+	
+	public static final Value v(final double value) {
+		return new Value(value);
+	}
+	
+	public static final Variable variable(final String name) {
+		return new Variable(name);
+	}
+	
+	public static final Sum sum(final Concatenation concatenation) {
+		return sum(concatenation, concatenation.getIndex(), v(concatenation.getOperands().size()));
+	}
+	
+	public static final Sum sum(final Object expression, final Object variable, final Object count) {
+		return new Sum(expression(expression), (Variable) expression(variable), expression(count));
+	}
+	
+	public static final Sum add(final Object... objects) {
+		return sum(concatenate(objects));
+	}
+	
+	public static final Product product(final Concatenation concatenation) {
+		return product(concatenation, concatenation.getIndex(), v(concatenation.getOperands().size()));
+	}
+	
+	public static final Product product(final Object expression, final Object variable, final Object count) {
+		return new Product(expression(expression), (Variable) expression(variable), expression(count));
+	}
+	
+	public static final Product multiply(final Object... objects) {
+		return product(concatenate(objects));
+	}
+	
+	public static final Sum subtract(final Object left, final Object right) {
+		return add(left, multiply(MINUS_ONE, right));
+	}
+	
+	public static final Product divide(final Object left, final Object right) {
+		return multiply(left, invert(right));
+	}
+	
+	public static final Concatenation concatenate(final Object... objects) {
+		final Concatenation result = new Concatenation();
+		
+		result.getOperands().addAll(stream(objects).map(MiniCAS2::expression).collect(toList()));
+		
+		return result;
+	}
+	
+	public static final Expression expression(final Object... objects) {
+		if (objects.length == 1) {
+			final Object object = objects[0];
+			
+			return object instanceof Expression ? (Expression) object :
+				object instanceof String ? variable(object.toString()) :
+					object instanceof Number ? v(((Number) object).doubleValue()) : null;
+		}
+		
+		return concatenate(objects);
+	}
+	
+	public static final Inverse invert(final Object operand) {
+		return new Inverse(expression(operand));
+	}
+	
+	public static final Cos cos(final Object operand) {
+		return new Cos(expression(operand));
 	}
 	
 	/**
@@ -73,21 +153,10 @@ public final class MiniCAS2 {
 	 */
 	public static final class Value implements Expression {
 		
-		private final double[] data;
+		private final double value;
 		
-		private final Variable index;
-		
-		public Value(final double... data) {
-			this.data = data;
-			this.index = new Variable("");
-		}
-		
-		public final Variable getIndex() {
-			return this.index;
-		}
-		
-		public final int getElementCount() {
-			return this.data.length;
+		public Value(final double value) {
+			this.value = value;
 		}
 		
 		@Override
@@ -97,12 +166,15 @@ public final class MiniCAS2 {
 		
 		@Override
 		public final double getAsDouble() {
-			return this.data[(int) this.getIndex().getAsDouble()];
+			return this.value;
 		}
 		
 		@Override
 		public final String toString() {
-			return join(", ", stream(this.data).mapToObj(Double::toString).toArray());
+			final long longValue = (long) this.getAsDouble();
+			
+			return longValue == this.getAsDouble() ? Long.toString(longValue) :
+				String.format(Locale.ENGLISH, "%.3f", this.getAsDouble());
 		}
 		
 		private static final long serialVersionUID = -7428349794913779700L;
@@ -161,6 +233,60 @@ public final class MiniCAS2 {
 		
 		public abstract List<Expression> getOperands();
 		
+		/**
+		 * @author codistmonk (creation 2015-04-06)
+		 */
+		public static abstract class Abstract implements Operation {
+			
+			private final List<Expression> operands;
+			
+			protected Abstract(final Expression... operands) {
+				this(asList(operands));
+			}
+			
+			protected Abstract(final List<Expression> operands) {
+				this.operands = operands;
+			}
+			
+			@Override
+			public final List<Expression> getOperands() {
+				return this.operands;
+			}
+			
+			private static final long serialVersionUID = 4769691796254099201L;
+			
+		}
+		
+	}
+	
+	/**
+	 * @author codistmonk (creation 2015-04-06)
+	 */
+	public static final class Concatenation extends Operation.Abstract {
+		
+		private final Variable index;
+		
+		public Concatenation() {
+			super(new ArrayList<>());
+			this.index = new Variable("");
+		}
+		
+		public final Variable getIndex() {
+			return this.index;
+		}
+		
+		@Override
+		public final double getAsDouble() {
+			return this.getOperands().get((int) this.getIndex().getAsDouble()).getAsDouble();
+		}
+		
+		@Override
+		public final String toString() {
+			return join(", ", this.getOperands());
+		}
+		
+		private static final long serialVersionUID = 8611209860000779554L;
+		
 	}
 	
 	/**
@@ -183,17 +309,10 @@ public final class MiniCAS2 {
 		/**
 		 * @author codistmonk (creation 2015-04-06)
 		 */
-		public static abstract class Abstract implements Iteration {
-			
-			private final List<Expression> operands;
+		public static abstract class Abstract extends Operation.Abstract implements Iteration {
 			
 			protected Abstract(final Expression expression, final Variable variable, final Expression count) {
-				this.operands = Arrays.asList(expression, variable, count);
-			}
-			
-			@Override
-			public final List<Expression> getOperands() {
-				return this.operands;
+				super(expression, variable, count);
 			}
 			
 			@Override
@@ -202,7 +321,24 @@ public final class MiniCAS2 {
 			}
 			
 			protected String protectedToString() {
-				return "(" + this.getExpression() + ")_" + this.getVariable() + "<" + this.getCount();
+				final StringBuilder resultBuilder = new StringBuilder();
+				final Expression expression = this.getExpression();
+				final boolean addPrentheses = expression instanceof Value || expression instanceof Variable;
+				
+				if (addPrentheses) {
+					resultBuilder.append("(");
+				}
+				
+				resultBuilder.append(expression);
+				
+				if (addPrentheses) {
+					resultBuilder.append(")");
+				}
+				
+//				resultBuilder.append("_0≤").append(this.getVariable()).append("<").append(this.getCount());
+				resultBuilder.append("_").append(this.getVariable()).append("<").append(this.getCount());
+				
+				return resultBuilder.toString();
 			}
 			
 			private static final long serialVersionUID = 56516015189794650L;
@@ -215,10 +351,6 @@ public final class MiniCAS2 {
 	 * @author codistmonk (creation 2015-04-05)
 	 */
 	public static final class Sequence extends Iteration.Abstract {
-		
-		public Sequence(final Value value) {
-			super(value, value.getIndex(), new Value(value.getElementCount()));
-		}
 		
 		public Sequence(final Expression expression, final Variable variable,
 				final Expression count) {
@@ -259,6 +391,12 @@ public final class MiniCAS2 {
 		
 		@Override
 		protected final String protectedToString() {
+			final Concatenation concatenationOperand = cast(Concatenation.class, this.getExpression());
+			
+			if (concatenationOperand != null && concatenationOperand.getIndex() == this.getVariable()) {
+				return "(" + join("+", concatenationOperand.getOperands()) + ")";
+			}
+			
 			return "Σ" + super.protectedToString();
 		}
 		
@@ -291,10 +429,62 @@ public final class MiniCAS2 {
 		
 		@Override
 		protected final String protectedToString() {
+			final Concatenation concatenationOperand = cast(Concatenation.class, this.getExpression());
+			
+			if (concatenationOperand != null && concatenationOperand.getIndex() == this.getVariable()) {
+				return "(" + join(" ", concatenationOperand.getOperands()) + ")";
+			}
+			
 			return "Π" + super.protectedToString();
 		}
 		
 		private static final long serialVersionUID = 1115752578667847713L;
+		
+	}
+	
+	/**
+	 * @author codistmonk (creation 2015-04-06)
+	 */
+	public static final class Inverse extends Operation.Abstract {
+		
+		public Inverse(final Expression operand) {
+			super(operand);
+		}
+		
+		@Override
+		public final double getAsDouble() {
+			return 1.0 / this.getOperands().get(0).getAsDouble();
+		}
+		
+		@Override
+		public final String toString() {
+			return "(1/" + this.getOperands().get(0) + ")";
+		}
+		
+		private static final long serialVersionUID = 5281325055444988205L;
+		
+	}
+	
+	/**
+	 * @author codistmonk (creation 2015-04-06)
+	 */
+	public static final class Cos extends Operation.Abstract {
+		
+		public Cos(final Expression operand) {
+			super(operand);
+		}
+		
+		@Override
+		public final double getAsDouble() {
+			return Math.cos(this.getOperands().get(0).getAsDouble());
+		}
+		
+		@Override
+		public final String toString() {
+			return "(cos " + this.getOperands().get(0) + ")";
+		}
+		
+		private static final long serialVersionUID = 2502032154950506484L;
 		
 	}
 	
