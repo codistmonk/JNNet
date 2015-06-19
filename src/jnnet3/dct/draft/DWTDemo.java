e7ed21de253d5d74a76fef8c6b35426ed32efa50
codistmonk <codistmonk@users.noreply.github.com>
2015-06-19 08:06:46 +0200

    [jnnet3][dct][draft][DWTDemo] Add multidimensional demo.

--- src/jnnet3/dct/draft/DWTDemo.java
+++ src/jnnet3/dct/draft/DWTDemo.java
@@ -27,6 +27,7 @@ public final class DWTDemo {
 	public static final void main(final String[] commandLineArguments) {
 		demo1D();
 		demo2D();
+		demoND();
 	}
 	
 	public static final void demo1D() {
@@ -76,6 +77,42 @@ public final class DWTDemo {
 		}
 	}
 	
+	public static final void demoND() {
+		final int[] ns = { 3, 3, 3 };
+		final double[] values = Arrays.stream(intRange(product(ns))).mapToDouble(x -> x).toArray();
+		values[values.length / 2] *= -1.0;
+		final BasicMatrix f = newColumn(values);
+		
+		debugPrint(Arrays.toString(ns));
+		
+		final BasicMatrix m = newIDWTMatrix(ns);
+		
+		if (f.size() < 50) {
+			debugPrint(m);
+		}
+		
+		final int rank = m.getRank();
+		
+		if (rank != f.size()) {
+			debugError(rank);
+		}
+		
+		final BasicMatrix g = m.solve(f);
+		
+		debugPrint(f);
+		debugPrint(g);
+		
+		{
+			final double[] normalizedXs = new double[ns.length];
+			
+			for (final int[] xs : cartesian(bounds(ns))) {
+				normalize(xs, ns, normalizedXs);
+				
+				debugPrint(Arrays.toString(xs), g.transpose().multiplyRight(newWaveColumn(ns, normalizedXs)).doubleValue(0));
+			}
+		}
+	}
+	
 	public static final BasicMatrix newColumn(final double... values) {
 		final int n = values.length;
 		final MatrixBuilder<Double> builder = PrimitiveMatrix.getBuilder(n, 1);
@@ -111,6 +148,89 @@ public final class DWTDemo {
 		}
 		
 		return builder.build();
+	}
+	
+	public static final BasicMatrix newIDWTMatrix(final int[] ns) {
+		final int n = product(ns);
+		final double[] normalizedXs = new double[ns.length];
+		final MatrixBuilder<Double> builder = PrimitiveMatrix.getBuilder(n, n);
+		final Iterable<int[]> tuples = cartesian(bounds(ns));
+		int i = -1;
+		
+		for (final int[] xs : tuples) {
+			++i;
+			int j = -1;
+			
+			normalize(xs, ns, normalizedXs);
+			
+			for (final int[] ks : tuples) {
+				++j;
+				
+				builder.set(i, j, waveCell(normalizedXs, ks));
+			}
+		}
+		
+		return builder.build();
+	}
+	
+	public static final void normalize(final int[] input, final int[] ns, final double[] output) {
+		final int n = input.length;
+		
+		for (int i = 0; i < n; ++i) {
+			output[i] = (double) input[i] / ns[i];
+		}
+	}
+	
+	public static final int[] bounds(final int... sizes) {
+		final int n = sizes.length;
+		final int[] result = new int[2 * n];
+		
+		for (int i = 0; i < n; ++i) {
+			result[2 * i + 1] = sizes[i] - 1;
+		}
+		
+		return result;
+	}
+	
+	public static final int sum(final int... values) {
+		return Arrays.stream(values).sum();
+	}
+	
+	public static final int product(final int... values) {
+		return Arrays.stream(values).reduce(1, (x, y) -> x * y);
+	}
+	
+	public static final double waveCell(final double[] xs, final int[] ks) {
+		final int m = xs.length;
+		final int n = 1 + sum(ks);
+		final int count = 1 << (m - 1);
+		double result = 0.0;
+		
+		for (int i = 0; i < count; ++i) {
+			double argument = ks[0] * xs[0] / n;
+			
+			for (int j = 1; j < m; ++j) {
+				final int signum = ((i >> (j - 1)) & 1) * 2 - 1;
+				argument += signum * ks[j] * xs[j] / n;
+			}
+			
+			result += wave(n, argument);
+		}
+		
+		return result;
+	}
+	
+	public static final BasicMatrix newWaveColumn(final int[] ns, final double[] xs) {
+		final int n = product(ns);
+		final MatrixBuilder<Double> builder = PrimitiveMatrix.getBuilder(n, 1);
+		int i = -1;
+		
+		for (final int[] ks : cartesian(bounds(ns))) {
+			builder.set(++i, 0, waveCell(xs, ks));
+		}
+		
+		return builder.build();
+		
 	}
 	
 	public static final BasicMatrix newIDWTMatrix(final int n1, final int n2) {
